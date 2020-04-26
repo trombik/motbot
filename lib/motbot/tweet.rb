7571eca28984d0695f8793eea9cd50928b7ee7aa
Tomoyuki Sakurai <y@trombik.org>
2020-04-27 04:53:15 +0900

    imp: create Motbot::Tweet

---
+++ lib/motbot/tweet.rb
@@ -0,0 +1,44 @@
+# frozen_string_literal: true
+
+require "yaml"
+
+module Motbot
+  # Class that represents a tweet
+  class Tweet
+    # Standard error in this class
+    class Error < StandardError; end
+
+    attr_accessor :status, :possibly_sensitive, :lat, :long, :place_id,
+                  :display_coordinates, :card_uri, :media_files, :timestamp
+
+    # The constructor.
+    #
+    # @param path Path to YAML file of the tweet
+    #
+    def initialize(path)
+      @path = path
+      t = load_yaml(@path)
+      t.each_key do |key|
+        instance_variable_set("@#{key}", t[key])
+      end
+      validate
+    end
+
+    # Load YAML file and returns a hash.
+    #
+    # @param Path to YAML file of the tweet
+    #
+    def load_yaml(path)
+      YAML.load_file(path).transform_keys(&:to_sym)
+    end
+
+    # Validate the instance, and raise Tweet::Error if the tweet is invalid
+    #
+    def validate
+      return true if !timestamp.nil? && !status.nil?
+
+      raise Motbot::Tweet::Error,
+            format("timestamp and status are required: %<path>s", path: @path)
+    end
+  end
+end
