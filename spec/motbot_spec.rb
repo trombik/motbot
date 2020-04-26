3a907a115c7379fe65dbb6d671caea8d42f1d74c
Tomoyuki Sakurai <y@trombik.org>
2020-04-27 04:58:24 +0900

    imp: initial implementation

--- spec/motbot_spec.rb
+++ spec/motbot_spec.rb
@@ -1,11 +1,9 @@
 # frozen_string_literal: true
 
+require "motbot"
+
 RSpec.describe Motbot do
   it "has a version number" do
     expect(Motbot::VERSION).not_to be nil
-  end
-
-  it "does something useful" do
-    expect(false).to eq(true)
   end
 end
