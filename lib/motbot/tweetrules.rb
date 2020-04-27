cdbc8a28efdee9bea37e52703e9a73caa1f7c6bd
Tomoyuki Sakurai <y@trombik.org>
2020-04-28 06:58:18 +0900

    imp: introduce Motbot::TweetRules
    validates media_files as well

---
+++ lib/motbot/tweetrules.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+module Motbot
+  # Rules that a tweet must follow
+  module TweetRules
+    # Must have a timestamp
+    def timestamp?
+      if meta["timestamp"].nil? || meta["timestamp"].to_s.empty?
+        warn "timestamp is required"
+        return false
+      end
+      true
+    end
+
+    # Must have corresponding media files
+    def valid_media_files?
+      result = true
+      media_files.each do |f|
+        file = Pathname.new(@path).parent.parent + "media" + f
+        unless file.exist? && file.file? && file.readable?
+          warn "#{file} in media_files must exist, be a file, and be readable"
+          result = false
+        end
+      end
+      result
+    end
+  end
+end
