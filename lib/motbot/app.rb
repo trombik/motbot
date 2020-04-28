4b74412e6f0431d73db22fb24b83786bc5538f3e
Tomoyuki Sakurai <y@trombik.org>
2020-04-28 10:13:09 +0900

    bugfix: reduce ABC size in `run`

--- lib/motbot/app.rb
+++ lib/motbot/app.rb
@@ -41,15 +41,19 @@ module Motbot
 
     # Post tweets.
     def run
-      load_tweets(@config["assets"]["tweet"]["path"]).each do |tweet|
-        next if tweet.meta.key?("state") && tweet.meta["state"] == "disabled"
-
+      tweets = load_tweets(@config["assets"]["tweet"]["path"]).reject { |t| disabled?(t) }
+      tweets.each do |tweet|
         if !tweet.media_files.empty?
           update_with_media(tweet)
         else
           @client.update(tweet.status_str)
         end
       end
+    end
+
+    # true if the tweet is disabled
+    def disabled?(tweet)
+      tweet.meta.key?("state") && tweet.meta["state"] == "disabled"
     end
 
     # Post a tweet with media
