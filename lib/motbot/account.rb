f72fc14ec969f66bc4600bd86656fd1758b8eb40
Tomoyuki Sakurai <y@trombik.org>
2020-05-02 16:49:14 +0900

    feature: support multiple languages

--- lib/motbot/account.rb
+++ lib/motbot/account.rb
@@ -5,11 +5,12 @@ require "twitter"
 module Motbot
   # Account that represents Twitter account to post tweets to.
   class Account
-    attr_reader :secret, :token, :client
+    attr_reader :secret, :token, :lang, :client
 
     def initialize(args)
       @secret = args[:secret]
       @token = args[:token]
+      @lang = args[:lang]
       @client = Twitter::REST::Client.new do |config|
         config.consumer_key        = ENV["TWITTER_CONSUMER_KEY"]
         config.consumer_secret     = ENV["TWITTER_CONSUMER_SECRET"]
